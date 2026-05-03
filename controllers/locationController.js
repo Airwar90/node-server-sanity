f583effbdb2a5c893578d510b031e3994d0177c4
Marta Grazia <airwar90@gmail.com>
2026-05-03 15:08:32 +0100

    since payload is built correctly in the frontend removed restructuring from the controller

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -4,33 +4,11 @@ const client = require('../config/sanityConfig')
 // @route POST /api/locations
 const createLocation = async (req, res, next) => {
   try {
-    const data = req.body
-
-    const doc = {
-      _type: 'activeLocation',
-      placeName: data.placeName,
-      gaelicName: data.gaelicName,
-      what3words: data.what3words,
-      region: data.region,
-      geopoint: {
-        _type: 'geopoint',
-        lat: data.lat,
-        lng: data.lng
-      },
-      locationType: data.locationType,
-      swimLocationType: data.swimLocationType,
-      entryType: data.entryType,
-      entrySurface: data.entrySurface,
-      postcode: data.postcode,
-      description: data.description,
-    }
-
-    const result = await client.create(doc)
-
-    res.status(201).json(result)
-  } catch (error) {
-    next(error)
-  }
+    const result = await client.create(req.body)
+        res.status(201).json(result)
+      } catch (error) {
+        next(error)
+      }
 }
 
 module.exports = { createLocation }
