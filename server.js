51b9b8a94301449c2a9f41d42e1196cde046246e
Marta Grazia <airwar90@gmail.com>
2026-05-03 14:51:18 +0100

    added cross origin resource sharing for frontend address

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 
 const { errorHandler } = require('./middleware/errorMiddleware')
 
@@ -7,6 +8,9 @@ const port = process.env.PORT || 5000
 console.log(process.env.SANITY_PROJECT_ID)
 const app = express()
 
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
