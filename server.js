57b850473748cdf28d9aed442d2c406cda7bf76b
Marta Grzia <23011076@uhi.ac.uk>
2026-05-19 13:47:04 +0100

    added image controller and paths to upload image to sanity and send image reference back to client

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const app = express()
 app.use(cors({
     origin: process.env.CLIENT_URL
 }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({limit: '10mb'}))
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
 //NEW ROUTES
 app.use('/api/locations', require('./routes/locationRoutes'))
 app.use('/api/services', require('./routes/serviceRoutes'))
+app.use('/api/uploadImage', require('./routes/imageRoutes'))
 
 app.use(errorHandler)
 
