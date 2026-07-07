16e78711b8d0a743fe013ab81711b28a58c748bf
Marta Grzia <23011076@uhi.ac.uk>
2026-07-07 11:41:10 +0100

    added create slug methods and slug addition to payload in service and location controllers

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -3,8 +3,24 @@ const client = require('../config/sanityConfig')
 // @desc Create Active Location
 // @route POST /api/locations
 const createLocation = async (req, res, next) => {
+  function createSlug(text) {
+    return text
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-')
+      .slice(0,96)
+  }
+  if(typeof req.body.placeName !== 'string' || !req.body.placeName.trim()) {
+    return res.status(400).json({error: "Place Name is required and it must be string"})
+  }
+  const newBody = { 
+    ...req.body, 
+    slug: {_type: 'slug', current: createSlug(req.body.placeName) } 
+  }
   try {
-    const result = await client.create(req.body)
+    const result = await client.create(newBody)
         res.status(201).json(result)
       } catch (error) {
         next(error)
--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -3,9 +3,26 @@ const client = require('../config/sanityConfig')
 // @desc Create Service
 // @route POST /api/services
 const createService = async (req, res, next) => {
+  function createSlug(text) {
+    return text
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+      .replace(/-+/g, '-')
+      .slice(0,96)
+  }
+  if(typeof req.body.name !== 'string' || !req.body.name.trim()) {
+    return res.status(400).json({error: "Name is required and it must be string"})
+  }
   console.log("createService hit", JSON.stringify(req.body, null, 2))
+  const newBody = { 
+    ...req.body, 
+    slug: {_type: 'slug', current: createSlug(req.body.name) }, 
+    isPublished: false 
+  }  
   try {
-    const result = await client.create(req.body)
+    const result = await client.create(newBody)
     res.status(201).json(result)
   } catch (error) {
     console.log("Sanity error", error)
