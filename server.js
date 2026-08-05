1494d9069b8810b6658f32317fa83f53314f464f
Marta Grazia <airwar90@gmail.com>
2026-08-05 11:30:19 +0100

    adjusted hop count to 3 as ip chain retrieved 2 addresses in front of mine during ip detection test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json({limit: '10mb'}))
 app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
-app.set('trust proxy', 1)
+app.set('trust proxy', 3)
 
 const rateLimit = require('express-rate-limit')
 
